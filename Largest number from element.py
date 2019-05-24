e212f22d2f1fe2fed3b1f67b773fd0a24f905f1e
Lakshmi.R <46350353+Lakshmi1098@users.noreply.github.com>
2019-05-24 09:37:31 +0530

    Update Largest number from element.py

--- Largest number from element.py
+++ Largest number from element.py
@@ -2,3 +2,12 @@ a=list(map(int,input().split()))
 a.sort(reverse=True)
 for i in a:
     print(i,end="")
+
+    
+    
+    ANOTHER METHOD:
+        a=[23,34,55]
+        a.sor(reverse=True)
+        res=[str(i) for i in a]
+        r=int("".join(res))
+        print(res)
