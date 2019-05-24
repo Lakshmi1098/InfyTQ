b12e885f8341d3afb942e3f15064fe8273ea5c0a
Lakshmi.R <46350353+Lakshmi1098@users.noreply.github.com>
2019-05-24 17:15:50 +0530

    Create Assigment 45.py

---
+++ Assigment 45.py
@@ -0,0 +1,11 @@
+def nearest_palindrome(number):
+    f=True
+    while(f):
+        number+=1
+        s=str(number)
+        if(s==s[-1::-1]):
+            f= False
+    return number
+
+number=12300
+print(nearest_palindrome(number))
