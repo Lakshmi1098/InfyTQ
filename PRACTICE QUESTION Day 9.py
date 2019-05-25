af0442b32ff4e463b92603e52155e1a02bd0d0c9
Lakshmi.R <46350353+Lakshmi1098@users.noreply.github.com>
2019-05-25 16:04:17 +0530

    PROB 11-15

---
+++ PRACTICE QUESTION Day 9.py
@@ -0,0 +1,66 @@
+********************** PROBLEM 11:*****************
+def find_upper_and_lower(sentence):
+    c1=0
+    c2=0
+    result_list=[]
+    for i in sentence:
+        if i.islower():
+            c1=c1+1
+        elif i.isupper():
+            c2=c2+1
+    result_list.append(c2)
+    result_list.append(c1)
+    return result_list
+
+sentence="Welcome to Mysore"
+print(find_upper_and_lower(sentence))
+
+
+
+****************** PROBLEM 12*******************
+def generate_sentences(subjects,verbs,objects):
+    sentence_list=[]
+    for i in subjects:
+        for j in verbs:
+	        for k in objects:
+	            sentence_list.append(i+" "+j+" "+k)
+    return sentence_list
+
+subjects=["I","You"]
+verbs=["love", "play"]
+objects=["Hockey","Football"]
+print(generate_sentences(subjects,verbs,objects))
+
+
+
+
+*****************PROBLEM 13:********************
+
+
+
+******************PROBLEM 14:*********************
+def find_five_digit():
+    num2=0
+    num3=0
+    num4=0
+    num5=0
+    for i in range(9,-1,-1):
+        num2=int(i-2)
+        num3=int(num2-2)
+        num4=int(num3-2)
+        num5=int(num3)
+        if(num3+num4+num5==i and num2+num3+num4+num5+i==19):
+            break
+    s=str(i)+str(num2)+str(num3)+str(num4)+str(num5)
+    print(s)
+
+find_five_digit()
+
+
+*****************PROBLEM 15:*************************
+def check_22(num_list):
+    for i in range(0,len(num_list)-1):
+        if(num_list[i]==2 and num_list[i+1]==2):
+            return True
+    return False
+print(check_22([3,2,5,1,2,1,2,2]))
