446d2107724a4b754aeda9229cfecaf82223c6b4
Lakshmi.R <46350353+Lakshmi1098@users.noreply.github.com>
2019-05-25 15:24:46 +0530

    PROB 6-10

--- PRACTICE QUESTION Day 9.py
+++ PRACTICE QUESTION Day 9.py
@@ -60,3 +60,22 @@ def generate_dict(number):
 
 number=10
 print(generate_dict(number))
+
+
+
+******************PROBLEM 10:******************
+def find_upper_and_lower(sentence):
+    c1=0
+    c2=0
+    result_list=[]
+    for i in sentence:
+        if i.islower():
+            c1=c1+1
+        elif i.isupper():
+            c2=c2+1
+    result_list.append(c2)
+    result_list.append(c1)
+    return result_list
+
+sentence="Welcome to Mysore"
+print(find_upper_and_lower(sentence))
