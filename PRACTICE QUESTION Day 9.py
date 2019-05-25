0e21937e81b511a6dea0162aa5fe680a00f6e3e5
Lakshmi.R <46350353+Lakshmi1098@users.noreply.github.com>
2019-05-25 15:25:47 +0530

    Update PRACTICE QUESTION Day 9.py

--- PRACTICE QUESTION Day 9.py
+++ PRACTICE QUESTION Day 9.py
@@ -64,18 +64,13 @@ print(generate_dict(number))
 
 
 ******************PROBLEM 10:******************
-def find_upper_and_lower(sentence):
-    c1=0
-    c2=0
-    result_list=[]
-    for i in sentence:
-        if i.islower():
-            c1=c1+1
-        elif i.isupper():
-            c2=c2+1
-    result_list.append(c2)
-    result_list.append(c1)
-    return result_list
+def string_both_ends(input_string):
+	if(len(input_string)<2):
+	    return -1
+	else:
+	    s=""
+	    s=input_string[0:2]+input_string[len(input_string)-2:len(input_string)]
+	    return s
 
-sentence="Welcome to Mysore"
-print(find_upper_and_lower(sentence))
+input_string="w3"
+print(string_both_ends(input_string))
