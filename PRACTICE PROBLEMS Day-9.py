a57881750f7ce6bc41b972bd30a01ef2ec1c41ec
Lakshmi.R <46350353+Lakshmi1098@users.noreply.github.com>
2019-05-27 02:22:25 +0530

    PROB 26-30

--- PRACTICE PROBLEMS Day-9.py
+++ PRACTICE PROBLEMS Day-9.py
@@ -13,3 +13,49 @@ string="Jet on the Mat but mat is too long"
 print(check_occurence(string))
 
 
+
+
+******************************PROBLEM 27:**************************************
+ def check_for_ten(num1,num2):
+    if(num1 ==10 or num2 ==10):
+        return True 
+    elif num1+num2==10:
+        return True 
+    else:
+        return False
+    
+print(check_for_ten(10,9))   
+    
+    
+    
+**************************PROBLEM 28:****************************************
+   def sing_99_bottles():
+   for i in range(99,-1,-1):
+       print(str(i)+"bottles of beer on the wall"+str(i)+"bottles of beer."+"\n"+
+             "Take one down, pass it around,"+str(i-1)+" bottles of beer on the wall.")
+sing_99_bottles()
+
+
+
+*****************************PROBLEM 29:*************************************
+    def exchange_list(number_list):
+    l=[]
+    s=[]
+    k=len(number_list)//2
+    for i in range(0,k):
+        l.append(number_list[i])
+    for i in range(k,len(number_list)):
+        s.append(number_list[i])
+        s.sort(reverse=True)
+    number_list=s+l
+    return number_list
+     
+number_list=[1,2,3,4,5,6,7,8,9,10]
+print(exchange_list(number_list))
+
+
+
+
+*****************************PROBLEM 30:*****************************************8
+   
+#Provide different values for limit and t
